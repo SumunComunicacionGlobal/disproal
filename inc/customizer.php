9092041e7098edb192afee11e4ea929fa03c2337
Fernando Blasco Martín <37153521+FernandoSumun@users.noreply.github.com>
2022-06-07 12:16:20 +0200

    New setting - URL catálogo

--- inc/customizer.php
+++ inc/customizer.php
@@ -59,3 +59,92 @@ function disproal_customize_preview_js() {
 	wp_enqueue_script( 'disproal-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'disproal_customize_preview_js' );
+
+
+/**
+* Crear panel de opciones en el customizador
+*/
+function sumun_new_customizer_settings($wp_customize) {
+    $web_title = get_bloginfo( 'name' );
+    // create settings section
+    $wp_customize->add_panel('sumun_opciones', array(
+        'title'         => $web_title . ': ' . __( 'Opciones de configuración', 'sumun-admin' ),
+        'description'   => __( 'Opciones para este sitio web', 'sumun-admin' ),
+        'priority'      => 1,
+    ));
+    // $wp_customize->add_section('sumun_redes_sociales', array(
+    //     'title'         => __( 'Redes sociales', 'sumun-admin' ),
+    //     'priority'      => 20,
+    //     'panel'         => 'sumun_opciones',
+    // ));
+    $wp_customize->add_section('sumun_ajustes', array(
+        'title'         => __( 'Otros ajustes', 'sumun-admin' ),
+        'priority'      => 20,
+        'panel'         => 'sumun_opciones',
+    ));
+
+
+
+    // $redes_sociales = array(
+    //     'email',
+    //     'whatsapp',
+    //     'linkedin',
+    //     'twitter',
+    //     'facebook',
+    //     'instagram',
+    //     'youtube',
+    //     'skype',
+    //     'pinterest',
+    //     'flickr',
+    //     'blog',
+    // );
+    // foreach ($redes_sociales as $red) {
+    //     // add a setting
+    //     $wp_customize->add_setting($red);
+        
+    //     // Add a control
+    //     $wp_customize->add_control( $red,   array(
+    //         'type'      => 'text',
+    //         'label'     => 'URL ' . $red,
+    //         'section'   => 'sumun_redes_sociales',
+    //     ) );
+    // }
+
+    // add a setting
+    $wp_customize->add_setting( 'url_catalogo', array(
+    	'capability'		=> 'edit_theme_options',
+    	'sanitize_callback'	=> 'disproal_sanitize_url',
+    ) );
+    
+    // Add a control
+    $wp_customize->add_control( 'url_catalogo',   array(
+        'type'      => 'url',
+        'label'     => 'Enlace al catálogo',
+        'section'   => 'sumun_ajustes',
+    ) );
+
+
+    // $paginas = array(
+    //     'contacto',
+    // );
+    // foreach ($paginas as $pagina) {
+
+    //     $wp_customize->add_setting( $pagina . '_id', array(
+    //       'capability' => 'edit_theme_options',
+    //       'sanitize_callback' => 'sumun_sanitize_dropdown_pages',
+    //     ) );
+
+    //     $wp_customize->add_control( $pagina . '_id', array(
+    //       'type' => 'dropdown-pages',
+    //       'section' => 'sumun_ajustes', // Add a default or your own section
+    //       'label' => __( 'Página de ' . $pagina ),
+    //     ) );
+
+    // }
+
+}
+add_action('customize_register', 'sumun_new_customizer_settings');
+
+function disproal_sanitize_url( $url ) {
+  return esc_url_raw( $url );
+}
